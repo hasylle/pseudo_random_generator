232774ca78d160b1501e4febed2d1f6534f1bcd6
hasylle <ellysah.guliman@gmail.com>
2018-09-06 21:54:55 +0800

    Changed hash algo to SHA-256

--- prg.py
+++ prg.py
@@ -1,5 +1,4 @@
 import hashlib
-import _md5
 from datetime import datetime
 
 
@@ -8,11 +7,11 @@ def pseudo_random(seed,N):
     k = 0;
     while k<N:
         now = datetime.now()
-        hash = _md5.md5(hash * now.microsecond).digest()
+        hash = hashlib.sha256(hash * now.microsecond).digest()
         for c in hash:
             print(c/255)
             break;
         k=k+1;
 
 if __name__ == '__main__':
-    pseudo_random(0.2, 100);
+    pseudo_random(0.2, 10);
