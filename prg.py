6759486eeaae13bf5ad0fb2a74336bd76ce54c5a
hasylle <ellysah.guliman@gmail.com>
2018-09-07 14:14:01 +0800

    Random function returns an array rand with N elements

--- prg.py
+++ prg.py
@@ -5,13 +5,16 @@ from datetime import datetime
 def pseudo_random(seed,N):
     hash = str(seed).encode()
     k = 0;
+    rand = [];
     while k<N:
         now = datetime.now()
         hash = hashlib.sha256(hash * now.microsecond).digest()
         for c in hash:
-            print(c/255)
+            rand.append(c/255)
             break;
         k=k+1;
-
+    return rand
 if __name__ == '__main__':
-    pseudo_random(0.2, 10);
+    rand = pseudo_random(0.2, 10);
+    for a in rand:
+        print(a)
