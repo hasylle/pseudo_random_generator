757e97e69744e9e530f62571c0d2d73f2e5b9a6f
Raphael Medina <raph@stormbenefits.com>
2018-09-16 07:30:52 +0800

    Implement Wilchman-Hill generator

--- prg.py
+++ prg.py
@@ -2,11 +2,13 @@ import hashlib
 import _md5
 from datetime import datetime
 
-def shift_lfsr(seed,mask_bits):
+
+def shift_lfsr(seed, mask_bits):
     lfsr = seed
     feedback = seed & 1
     while True:
         lfsr >>= 1
+        # print(feedback)
         if feedback == 1:
             lfsr ^= mask_bits
             break
@@ -15,38 +17,56 @@ def shift_lfsr(seed,mask_bits):
     return lfsr
 
 
-def pseudo_random(seed,N):
+def wilchman_hill(seed):
+    a, x = divmod(seed, 30268)
+    a, y = divmod(a, 30306)
+    a, z = divmod(a, 30322)
+    x = (171 * x) % 30269
+    y = (172 * y) % 30307
+    z = (170 * z) % 30323
+    ret_val = (x / 30269.0 + y / 30307.0 + z / 30323.0) % 1.0
+    print("Wilchman Hill: ", ret_val)
+    return (x / 30269.0 + y / 30307.0 + z / 30323.0) % 1.0
+
+
+def pseudo_random(seed, N):
     k = 0
-    rand = []
+    rand_list = []
+    m1 = 30269
+    m2 = 30307
+    m3 = 30323
     print(seed)
     shift_bits = seed % 5
-    c = (seed ^ seed << shift_bits) & 0xFFFFFF
-    print(c)
-    a = (seed ^ c >> shift_bits) & 0xFFFFFF
+    print(shift_bits)
+    x_n = (seed ^ seed << shift_bits) & 0xFFFFFF
+    a = (seed ^ x_n >> shift_bits) & 0xFFFFFF
     print(a)
-    while k<N:
-        # first algorithm -- linear congruential
-        rand.append((a * seed + c))
-        c = rand[k]
-        #second algorithm -- LFSR
-        lfsr = shift_lfsr(int(rand[k] * a), c)
-        lfsr = shift_lfsr(lfsr,c)
-        rand[k] = (lfsr & 0xffffff)
-        #insert 2nd algorithm here
+    while k < N:
+        """
+        First algorithm -- Wichman-Hill generator via linear congruential
+        Formula for Wichman-Hill generator:
+        rand = (x_n/m1 + y_n/m2 + z_n/m3) mod 1
+        """
+        random_number = wilchman_hill(seed)
+        # random_number = (random_number ^ random_number << shift_bits) & 0xFFFFFF
+        # random_number
+        rand_list.append(random_number)
+        random_number = rand_list[k]
 
-        #insert 3rd algorithm here
+        # Second algorithm -- LFSR
+        # lfsr = shift_lfsr(int(rand_list[k] * a), random_number)
+        # lfsr = shift_lfsr(lfsr, int(random_number))
+        # rand_list[k] = (lfsr & 0xffffff)
 
-        #finally convert range to [0,1]
-        rand[k] = rand[k] / 0xffffff
-        k+=1
+        # Finally, convert range to [0,1]
+        # rand_list[k] = rand_list[k] / 0xffffff
+        k += 1
     if N == 1:
-        return rand[0]
-    return rand
-
-
+        return rand_list[0]
+    return rand_list
 
 
 if __name__ == '__main__':
     input = datetime.now().microsecond
-    rand = pseudo_random(input, 10000)
+    rand = pseudo_random(input, 10)
     print(rand)
